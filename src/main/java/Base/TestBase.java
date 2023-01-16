a1297c1267916ab1e7d835894a481811fa371bdc
komalsraut <122728472+komalsraut@users.noreply.github.com>
2023-01-16 14:53:52 +0530

    Update TestBase.java

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
 	public static WebDriver driver;
 	
 public void initialization()throws Exception {
+	//launching chrome browser
 	WebDriverManager.chromedriver().setup();
 	 driver=new ChromeDriver();
 	driver.manage().window().maximize();
